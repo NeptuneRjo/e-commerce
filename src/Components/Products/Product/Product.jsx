fd20f8c2b0cc596cca33191c0128f0669e080660
NeptuneRjo <rjo9000@outlook.com>
2022-03-02 16:44:02 -0500

    added jsx with API data

--- src/Components/Products/Product/Product.jsx
+++ src/Components/Products/Product/Product.jsx
@@ -8,16 +8,18 @@ import useStyles from './styles';
 const Product = ({ product }) => {
     const classes = useStyles();
 
+    console.log(product.image.url)
+
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} img='' title={product.name} />
+            <CardMedia className={classes.media} image={product.image.url} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant='h5' gutterBottom>
                         {product.name}
                     </Typography>
                     <Typography variant='h5'>
-                        {product.price}
+                        {product.price.formatted_with_symbol}
                     </Typography>
                     <Typography variant='body2' color='textSecondary'>
                         {product.description}
