15744f0e471543a08cd8fa0346d570250a24c821
NeptuneRjo <rjo9000@outlook.com>
2022-03-02 21:07:19 -0500

    Cart properly displays item totals

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
+import useStyles from './styles';
+
 const Cart = ({ cart }) => {
-    const isEmpty = !cart.line_items.length;
+    const classes = useStyles();
+    // const isEmpty = !cart.line_items.length;
 
     const EmptyCart = () => (
         <Typography variant='subtitle1'>You have no items in your shopping cart, start adding some!</Typography>
@@ -22,18 +25,20 @@ const Cart = ({ cart }) => {
                     Subtotal: { cart.subtotal.formatted_with_symbol }
                 </Typography>
                 <div>
-                    <Button className={classes.emptyButton} size='large' type='button' variant='container' color='secondary'>Empty Cart</Button>
-                    <Button className={classes.checkoutButton} size='large' type='button' variant='container' color='primary'>Checkout</Button>
+                    <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
+                    <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
                 </div>
             </div>
         </>
     )
 
+    if (!cart.line_items) return 'Loading...';
+
     return (
         <Container>
             <div className={classes.toolbar} />
             <Typography className={classes.title} variant='h3' >Your Shopping Cart</Typography>
-            { isEmpty ? <EmptyCart /> : <FilledCard /> }
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
         </Container>
     )
 }
