6743614f7f3117ca9a6739a83310687f025a559d
NeptuneRjo <rjo9000@outlook.com>
2022-03-03 15:10:19 -0500

    created and passed cart handling functions

--- src/App.js
+++ src/App.js
@@ -18,9 +18,27 @@ const App = () => {
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);   
+    const { cart } = await commerce.cart.add(productId, quantity);   
 
-    setCart(item.cart);
+    setCart(cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  }
+
+  const handleRemovefromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+
+    setCart(cart);
   }
 
   useEffect(() => {
@@ -36,7 +54,14 @@ const App = () => {
         <Navbar totalItems={cart.total_items} />
         <Routes>
           <Route exact path='/' element={<Products products={products} onAddToCart={handleAddToCart}/>} />
-          <Route path='/cart' element={<Cart cart={cart} />} />
+          <Route path='/cart' element={
+            <Cart 
+              cart={cart} 
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemovefromCart={handleRemovefromCart}
+              handleEmptyCart={handleEmptyCart}
+            />} 
+          />
         </Routes>
       </div>
     </Router>
